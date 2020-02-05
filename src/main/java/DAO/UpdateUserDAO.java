cc8954fbd8a50b61b87e6132cbae8183868c7c8f
Mihai Daniel Adin <mihaidanieladin@gmail.com>
2020-02-05 14:34:50 +0200

    add-dao-basic-methods

--- src/main/java/DAO/UpdateUserDAO.java
+++ src/main/java/DAO/UpdateUserDAO.java
@@ -1,6 +1,36 @@
 
 package DAO;
 
-public class UpdateUserDAO {
+import Interface.Conection;
+import static Interface.Conection.SQL_INSERT_USER;
+import static Interface.Conection.SQL_PASSWORD;
+import static Interface.Conection.SQL_URL;
+import static Interface.Conection.SQL_USERNAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UpdateUserDAO implements Conection {
     
+    public int updateUser() throws ClassNotFoundException, SQLException {
+        Connection conection = null;
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        
+        try {
+            
+            // Initialize and register the JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conection = DriverManager.getConnection(SQL_URL, SQL_USERNAME, SQL_PASSWORD);
+
+            // Prepare the statement
+            statement = conection.prepareStatement(SQL_UPDATE_USER);
+            
+        } catch(SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        return 0;
+    } 
 }
